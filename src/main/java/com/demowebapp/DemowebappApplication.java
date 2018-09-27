bf32dfc99247512dfde635be783e61614917b9ed
paichayon321 <35794499+paichayon321@users.noreply.github.com>
2018-09-27 13:15:13 +0700

    Add files via upload

---
+++ src/main/java/com/demowebapp/DemowebappApplication.java
@@ -0,0 +1,12 @@
+package com.demowebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemowebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemowebappApplication.class, args);
+	}
+}
